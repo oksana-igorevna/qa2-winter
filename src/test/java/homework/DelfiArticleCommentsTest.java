8605927ec8951cd9d0a38b75b497a90d96887309
Oksana <oksikk3@gmail.com>
2021-12-19 17:17:27 +0200

    changes 3

---
+++ src/test/java/homework/DelfiArticleCommentsTest.java
@@ -0,0 +1,98 @@
+package homework;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class DelfiArticleCommentsTest {
+
+    private final By ACCEPT_COOKIE_BTN = By.xpath(".// button [@mode = 'primary']");
+    private final By HOME_PAGE_TITLE = By.xpath(".//h1 [contains(@class, 'headline__title')]");
+    private final By HOME_PAGE_COMMENTS = By.xpath(".//a[contains(@class, 'comment-count')]");
+    private final By HOME_PAGE_ARTICLE = By.tagName("article");
+
+    private final By ARTICLE_PAGE_TITLE = By.xpath(".//h1 [contains(@class, 'text-size-md-30')]");
+    private final By ARTICLE_PAGE_COMMENTS = By.xpath(".//a [contains(@class, 'text-size-md-28')]");
+
+
+
+    private WebDriver driver;
+
+    @Test
+    public void titleAndCommentsCountCheck() {
+        System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://delfi.lv");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIE_BTN));
+
+        driver.findElement(ACCEPT_COOKIE_BTN).click();
+
+        List<WebElement> articles = driver.findElements(HOME_PAGE_ARTICLE);
+        WebElement article = articles.get(6);
+
+
+        String homePageTitle = article.findElement(HOME_PAGE_TITLE).getText();
+        int homePageCommentsCount = getCommentsCount(article, HOME_PAGE_COMMENTS);
+
+        article.findElement(HOME_PAGE_TITLE).click();
+
+        String articlePageTitle = driver.findElement(ARTICLE_PAGE_TITLE).getText();
+        int articlePageCommentsCount = getCommentsCount(ARTICLE_PAGE_COMMENTS);
+
+
+        Assertions.assertEquals(homePageTitle, articlePageTitle, "Wrong title!");
+        Assertions.assertEquals(homePageCommentsCount, articlePageCommentsCount, " Wrong comments count!");
+
+    }
+    private int getCommentsCount(By locator) {
+        int commentsCount = 0;
+
+        if (!driver.findElements(locator).isEmpty()) {
+            commentsCount = removeBrackets(driver.findElement(locator));
+
+        }
+        return commentsCount;
+
+    }
+
+    private int getCommentsCount(WebElement we, By locator) {
+        int commentsCount = 0;
+
+        if (!we.findElements(locator).isEmpty()) {
+            commentsCount = removeBrackets(we.findElement(locator));
+
+        }
+        return commentsCount;
+
+    }
+
+    private int removeBrackets(WebElement we) {
+
+        String commentsCountText = we.getText();
+        commentsCountText = commentsCountText.substring(1, commentsCountText.length() - 1);
+        return Integer.parseInt(commentsCountText);
+
+    }
+
+
+
+    @AfterEach
+    public void closeBrowser() {
+        driver.close();
+
+    }
+}
